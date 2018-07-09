4da8aac447bbea4b27e3473dd2a1db4a1a35bb37
sayantanm19 <sayantanmaiti@gmail.com>
2018-07-09 22:32:58 +0530

    Added Modified password option.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-
 $(document).ready(function(){
         $(".login-prev-btn").click(function(){
         console.log('clicked');
         $(".inputx").show(100);
         $(".login-prev-btn").hide();
+    });
+
+    $(".forgot").click(function(){
+        console.log('forgot?');
+        $(".forgot").hide();
+        $(".hint").show(100);
     });
 });
